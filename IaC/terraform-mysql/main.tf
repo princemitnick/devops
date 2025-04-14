5a8388ce49a1959c35a3e77e3fadbb75ac8207ea
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-04-13 20:13:43 -0400

    edit resource docker_image mysql

--- IaC/terraform-mysql/main.tf
+++ IaC/terraform-mysql/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 } 
 
+variable "mysql_env" {
+  type = string({
+    internal = 3306
+    external = 3307
+
+    })
+}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
@@ -16,11 +24,11 @@ resource "docker_image" "mysql" {
 }
 
 resource "docker_container" "mysql_1" {
-  name = "mysql"
+  name = "mysql-1"
   image = docker_image.mysql.latest
   ports {
-    internal = 3306
-    external = 3307
+    internal = var.mysql_env.internal
+    external = var.mysql_env.external
   }
   env = [
     "MYSQL_ROOT_PASSWORD=rootpassword",
@@ -29,7 +37,7 @@ resource "docker_container" "mysql_1" {
 }
 
 resource "docker_container" "mysql_2" {
-  name = "mysql"
+  name = "mysql-2"
   image = docker_image.mysql.latest
   ports {
     internal = 3306
