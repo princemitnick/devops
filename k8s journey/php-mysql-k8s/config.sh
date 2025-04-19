c0cf1fd4ef7d2531964d012352e1b0dd8b37fd7f
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-04-19 16:40:39 -0400

    add ingress to php lab

--- k8s journey/php-mysql-k8s/config.sh
+++ k8s journey/php-mysql-k8s/config.sh
@@ -1,3 +1,31 @@
+"""
+✅ Contenu inclus :
+	•	php/ : ton app PHP (formulaire + connexion MySQL)
+	•	mysql-init/ : script d’initialisation SQL
+	•	Dockerfile : pour builder l’image PHP
+	•	k8s/ : les fichiers YAML pour les 8 étapes :
+
+
+Fichier
+Ã‰tape couverte
+php-deployment.yaml
+DÃ©ploiement app PHP
+mysql-deployment.yaml
+DÃ©ploiement MySQL + volume
+service-php.yaml
+Service interne PHP
+service-mysql.yaml
+Service interne MySQL
+configmap.yaml
+Configuration app (ex : nom de la base)
+secrets.yaml
+Credentials (user/pass DB)
+ingress.yaml
+Ingress Controller avec nom de domaine local
+(Ã  venir)
+Monitoring (Prometheus) + CI GitHub Actions
+"""
+
 eval $(minikube docker-env)
 
 docker build -t php-app:1.0 .
@@ -7,4 +35,12 @@ kubectl apply -f k8s/php-deployment.yaml
 kubectl apply -f k8s/service-php.yaml
 
 kubectl apply -f k8s/service-mysql.yaml
+
+
+minikube addons enable ingress
+
+sudo nano /etc/hosts
+#<minikube-ip>    php.local
+
+kubectl apply -f k8s/ingress.yaml
 
