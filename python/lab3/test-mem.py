a84aa926009b18e3807f2cedb1345db5cb5b2944
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-04-26 13:04:06 -0400

    python lab

--- python/lab3/test-mem.py
+++ python/lab3/test-mem.py
@@ -1,9 +1,30 @@
+import datetime
 import subprocess
-
-def mem_check():
-    res = subprocess.run(["free", "-m"], capture_output=True, text=True)
-    for line in res.stdout.splitlines():
-        print(line)
+import time
 
 
-mem_check()
+def disk_and_mem_check():
+    disk_free = None
+    free_mem = None
+    res_disk_check = subprocess.run(["df", "-m"], capture_output=True, text=True)
+    for line in res_disk_check.stdout.splitlines():
+        if "/dev/disk3s1s1" in line:
+            disk_free = line.split()[4]
+            if int(disk_free.lstrip()[0]) < 20:
+                print("⚠️ Avertissement : Espace de disque presqu'insuffisant")
+
+    res_mem_check = subprocess.run(["free", "-m"], capture_output=True, text=True)
+    for line in res_mem_check.stdout.splitlines():
+        if "Mem:" in line:
+            free_mem = int(line.split()[3])
+            if free_mem < 500:
+                print("Avertissement : Memoire disponible faible")
+
+    with open("system.log", "w") as f:
+        f.write(f"==========={datetime.date} - {datetime.time}===============\n")
+        f.write(f"Disk Usage : {disk_free}\n")
+        f.write(f"Mem Usage : \n")
+
+
+
+disk_and_mem_check()
