62e67bf27ad6acd83ed332d1b41a5fe0d86888fa
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-04-26 12:12:17 -0400

    Python lab

--- python/lab3/test-mem.py
+++ python/lab3/test-mem.py
@@ -3,6 +3,9 @@ import subprocess
 def mem_check():
     res = subprocess.run(["free", "-m"], capture_output=True, text=True)
     print(res.stdout)
+    for line in res.stdout:
+        if "free" in line:
+            print("yes")
 
 
 mem_check()
